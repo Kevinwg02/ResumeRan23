1b804d202b1e5a6e4348ac8b08dac437d0ad65f8
Kevinwg02 <kevin.waag@outlook.com>
2023-08-23 16:31:51 +0200

    logo for degrees | sidebar is a mess....

---
+++ assets/js/sidebarnav.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById(".sidenavbar").style.display = "block";
+    document.getElementById("exitsidenav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.display = "none";
+}
